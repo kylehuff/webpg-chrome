2cf18126f938d00aacc835f1d2b7464430fb0d24
kylehuff <code@curetheitch.com>
2013-01-21 11:25:50 -0500

    Changed update_translations.sh to be less project specific

--- update_translations.sh
+++ update_translations.sh
@@ -1,36 +1,43 @@
 #!/bin/bash
 
-# Creates required locale files for the webpg extension project (firefox/chrome)
+# Creates required locale files for the extension project (firefox/chrome)
 # Requirements:
 #   pybabel
 #   gettext
 
-BUGSADDR=webpg-translations@webpg.org
+PROJECT_NAME=webpg
+BUGSADDR=$PROJECT_NAME-translations@$PROJECT_NAME.org
 ORGANIZATION=CURETHEITCH
 AUTHOR="Kyle L. Huff"
 SRCDIR=extension
 PODIR=po
-POTFILE=webpg.pot
+POTFILE=$PROJECT_NAME.pot
 FFEXT=false
+
 if [ -a "chrome.manifest" ]; then
     FFEXT=true
 fi
 
 # Obtain the project version number and set the locale directory
 if ${FFEXT:=true}; then
-    PROJECT=webpg-firefox
+    PROJECT=$PROJECT_NAME-firefox
     LOCALEDIR="locale"
     VERSION=`sed -n 's/.*em\:version\=\"\(.*\)\"/\1/p' install.rdf`
 else
-    PROJECT=webpg-chrome
+    PROJECT=$PROJECT_NAME-chrome
     LOCALEDIR="extension/_locales"
     VERSION=`sed -n 's/.*\"version\"\:.\"\(.*\)\",.*/\1/p' extension/manifest.json`
 fi
 
 LOCALES=('ar' 'bg' 'ca' 'ca' 'cs' 'da' 'de' 'el' 'en' 'en_GB' 'en_US' 'es' 'es_419' 'et' 'fi' 'fil' 'fr' 'he' 'hi' 'hr' 'hu' 'id' 'it' 'ja' 'ko' 'lt' 'lv' 'nl' 'np' 'pl' 'pt_BR' 'pt_PT' 'ro' 'ru' 'sk' 'sl' 'sr' 'sv' 'th' 'tr' 'uk' 'vi' 'zh_CN' 'zh_TW')
 
+# Create the po directory if it does not exits
+if [ ! -d "$PODIR" ]; then
+    mkdir $PODIR
+fi
+
 # Extract the translatable strings from the source files
-pybabel extract -F webpg-babel.cfg --copyright-holder=$ORGANIZATION --msgid-bugs-address=$BUGSADDR --project=$PROJECT --version=$VERSION $SRCDIR > $PODIR/$POTFILE
+pybabel extract -F $PROJECT_NAME-babel.cfg --copyright-holder=$ORGANIZATION --msgid-bugs-address=$BUGSADDR --project=$PROJECT --version=$VERSION $SRCDIR > $PODIR/$POTFILE
 
 if ${FFEXT:=true}; then
     # Extract the translatable strings from the XUL files
@@ -48,7 +55,7 @@ do
         msgmerge --update --no-fuzzy-matching --backup=off $PODIR/$locale.po $PODIR/$POTFILE
     else
         echo "Creating new locale: $locale"
-        msginit --no-fuzzy-matching --no-translator -l $locale -i $PODIR/$POTFILE -o $PODIR/$locale.po
+        msginit --no-translator -l $locale -i $PODIR/$POTFILE -o $PODIR/$locale.po
     fi
 done
 
@@ -60,10 +67,10 @@ if ${FFEXT:=true}; then
             if [ ! -d "$LOCALEDIR/$locale" ]; then
                 mkdir -p $LOCALEDIR/$locale
             fi
-            echo "Creating $LOCALEDIR/$locale/webpg.dtd"
-            gawk 'BEGIN{ FS="\n"; RS="";} match($0, /msgid.*(\"(.*)\")$/, b) && match(b[0], /msgid.*(\"(.*)\")(\n)?msgstr.*(\".*\")(\n)?/, a) { if (a[2]!="") printf("<!ENTITY %s %s>\n", gensub(/[^\"|^a-Z|^0-9|^\.]/, "_", "g", a[2]), a[4]) }' $PODIR/$locale.po > $LOCALEDIR/$locale/webpg.dtd
-            echo "Creating $LOCALEDIR/$locale/webpg.properties"
-            gawk 'BEGIN{ FS="\n"; RS="";} match($0, /msgid.*(\"(.*)\")$/, b) && match(b[0], /msgid.*(\"(.*)\")(\n)?msgstr.*(\"(.*)\")(\n)?/, a) { if (a[2]!="") printf("%s=%s\n", gensub(/[^\"|^a-Z|^0-9|^\.]/, "_", "g", a[2]), a[5]) }' $PODIR/$locale.po > $LOCALEDIR/$locale/webpg.properties
+            echo "Creating $LOCALEDIR/$locale/$PROJECT_NAME.dtd"
+            gawk 'BEGIN{ FS="\n"; RS="";} match($0, /msgid.*(\"(.*)\")$/, b) && match(b[0], /msgid.*(\"(.*)\")(\n)?msgstr.*(\".*\")(\n)?/, a) { if (a[2]!="") printf("<!ENTITY %s %s>\n", gensub(/[^\"|^a-Z|^0-9|^\.]/, "_", "g", a[2]), a[4]) }' $PODIR/$locale.po > $LOCALEDIR/$locale/$PROJECT_NAME.dtd
+            echo "Creating $LOCALEDIR/$locale/$PROJECT_NAME.properties"
+            gawk 'BEGIN{ FS="\n"; RS="";} match($0, /msgid.*(\"(.*)\")$/, b) && match(b[0], /msgid.*(\"(.*)\")(\n)?msgstr.*(\"(.*)\")(\n)?/, a) { if (a[2]!="") printf("%s=%s\n", gensub(/[^\"|^a-Z|^0-9|^\.]/, "_", "g", a[2]), a[5]) }' $PODIR/$locale.po > $LOCALEDIR/$locale/$PROJECT_NAME.properties
         fi
     done
 else
