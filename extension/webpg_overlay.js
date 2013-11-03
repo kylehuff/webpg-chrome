e68e04da405c74cbf1befe78e3867d3c2ce71057
kylehuff <code@curetheitch.com>
2013-11-03 13:47:47 -0500

    Removed closure from scroll event function

--- extension/webpg_overlay.js
+++ extension/webpg_overlay.js
@@ -613,6 +613,6 @@ if (webpg.utils.detectedBrowser['vendor'] == "mozilla") {
     }
 } else {
     webpg.overlay.init();
-    webpg.jq(window).scroll(function(){webpg.overlay.init();});
+    window.addEventListener("scroll", webpg.overlay.init, true);
 }
 /* ]]> */
