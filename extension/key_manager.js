03b343f542457dcc108e8ece17c046c626d3f811
kylehuff <code@curetheitch.com>
2014-10-12 18:32:25 -0400

    Fix display of secondary key photos

--- extension/key_manager.js
+++ extension/key_manager.js
@@ -1582,6 +1582,7 @@ webpg.keymanager = angular.module("webpg.keymanager", [])
           webpg.plugin.gpgGetPhotoInfo(key.id, function(photo_info) {
             key.photos_provided = photo_info.photos_provided;
             key.photos = photo_info.photos;
+            key.photos_path = photo_info.photos_path;
 
             if (key.photos_provided > 0 && webpg.jq(e.target).hasClass("photo") === false) {
               // When a primary key accordion header is clicked open, do the
@@ -1710,7 +1711,7 @@ webpg.keymanager = angular.module("webpg.keymanager", [])
             if (key.photos_provided > 0) {
               jqueryElm.find('img.photo_img').each(function(e) {
                 webpg.jq(this).attr('src',
-                  'file:///tmp/key_photos/' + scrub(this.parentElement.id.split('photo-')[1]) + '.jpg?' + new Date().getTime());
+                  'file://' + key.photos_path + '/key_photos/' + scrub(this.parentElement.id.split('photo-')[1]) + '.jpg?' + new Date().getTime());
               });
             }
             scope.$apply();
